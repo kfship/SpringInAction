01be8d8e22f34c17e58ec709d6efdcb96305ca06
Woojung Kim <kfship@gmail.com>
2021-01-06 00:33:33 +0900

    CH3. JPA

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -4,16 +4,30 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "Taco_Order")
 public class Order {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private Date placedAt;
 
@@ -41,10 +55,17 @@ public class Order {
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
 	
+	@ManyToMany(targetEntity=Taco.class)
 	private List<Taco> tacos = new ArrayList<>();
 	
 	public void addDesign(Taco design) {
 		this.tacos.add(design);
 	}
+	
+	@PrePersist  //Taco 객체가 저장되기 전 실행돼서 placedAt 속성을 현재 일자와 시간으로 설정함
+	void placedAt() {
+		this.placedAt = new Date();
+	}
+	
 
 }
--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,9 +1,15 @@
 package tacos.data;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
 import tacos.Order;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 
-	Order save(Order order);
+	List<Order> findByDeliveryZip(String deliveryZip);
+	
+	//Order save(Order order);
 	
 }
